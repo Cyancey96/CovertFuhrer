e7ea90213ae416db8d07a901c2eb6dfe5a20f6a1
Obidike,Ike <iobidike@vertafore.com>
2018-10-04 15:48:35 -0600

    changed Games.cs

--- CovertFuhrerServer/CovertFuhrerServer/Game.cs
+++ CovertFuhrerServer/CovertFuhrerServer/Game.cs
@@ -1,23 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
 namespace CovertFuhrerServer
 {
     class Game
     {
         private List<Client> clients;
+        private int index;
+        static Random random = new Random();
         public Game(List<Client> clients)
         {
             this.clients = clients;
         }
-
         public void start()
         {
-            Console.WriteLine("Game has started with the following players:");
-            foreach (var client in clients)
+            Client.SendMessageToAllClients(Message.gameStart());
+            Client firstPresident = pickRandomPresident(clients);
+            Client nextPresident = rotatePresident(index);
+            chancellorOptions(firstPresident);
+            //firstPresident.SendPresidentialPacket();
+        }
+        // first president is randomly selected
+        public Client pickRandomPresident(List<Client> clients)
+        {
+            index = random.Next(clients.Count);
+            Message.becomePresident(clients[index].player);
+            return clients[index];
+        }
+        //the presidency moves to the next player in line
+        public Client rotatePresident(int number)
+        {
+            if (number != clients.Count - 1)
             {
-                Console.WriteLine(client.player.name);
+                number = number + 1;
+            }
+            else
+            {
+                number = 0;
+            }
+            Message.becomePresident(clients[number].player);
+            return clients[number];
+        }
+        public void chancellorOptions(Client president)
+        {
+            Console.WriteLine(president.player.name + ", kindly nominate a Chancellor from among the following: ");
+            foreach (var player in clients)
+            {
+                if (clients.IndexOf(player) != index)
+                {
+                    Console.WriteLine(player.player.name);
+                }
             }
         }
     }
