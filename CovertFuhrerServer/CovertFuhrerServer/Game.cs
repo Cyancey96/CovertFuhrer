ef8a5caaed49eec22c3ebc3bde1cf4a3876f738f
Obidike,Ike <iobidike@vertafore.com>
2018-10-04 15:03:29 -0600

    added a couple methods to game.cs

--- CovertFuhrerServer/CovertFuhrerServer/Game.cs
+++ CovertFuhrerServer/CovertFuhrerServer/Game.cs
@@ -10,6 +10,8 @@ namespace CovertFuhrerServer
 
         private int index;
 
+        static Random random = new Random();
+
         public Game(List<Client> clients)
         {
             this.clients = clients;
@@ -22,11 +24,50 @@ namespace CovertFuhrerServer
             {
                 Console.WriteLine(client.player.name);
             }
+
+           
+            Client firstPresident = pickRandomPresident(clients);
+            Client nextPresident = rotatePresident(index);
+
+            chancellorOptions(firstPresident);
+
+            //firstPresident.SendPresidentialPacket();
         }
 
-        private PlayerObject pickRandomPresident(List<Client> clients)
+        // first president is randomly selected
+        public Client pickRandomPresident(List<Client> clients)
         {
-            return PlayerObject 
+            index = random.Next(clients.Count);
+            Message.becomePresident(clients[index].player);
+            return clients[index]; 
+        }
+
+        //the presidency moves to the next player in line
+        public Client rotatePresident(int number)
+        {
+            if(number != clients.Count-1)
+            {
+                number = number + 1;
+            }
+            else
+            {
+                number = 0;
+            }
+
+            Message.becomePresident(clients[number].player);
+            return clients[number];
+        }
+
+        public void chancellorOptions(Client president)
+        {
+            Console.WriteLine(president.player.name + ", kindly nominate a Chancellor from among the following: ");
+            foreach (var player in clients)
+            {
+                if(clients.IndexOf(player) != index)
+                {
+                    Console.WriteLine(player.player.name);
+                }
+            }
         }
     }
 }
