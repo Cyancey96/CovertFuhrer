d672875cfb2bce873e57f45af405b76f59de170a
Cyancey96 <chris.yancey96@gmail.com>
2018-10-05 13:13:17 -0600

    Add files via upload

--- CovertFuhrerServer/CovertFuhrerServer/Game.cs
+++ CovertFuhrerServer/CovertFuhrerServer/Game.cs
@@ -7,18 +7,54 @@ namespace CovertFuhrerServer
     class Game
     {
         private List<Client> clients;
+
+        private TurnState state { get; set; }
+
+        private int liberalPolicyCount { get; set; }
+
+        private int facistPolicyCount { get; set; }
+
+        private int currentPresident { get; set; }
+
+        private int currentChancellor { get; set; }
+
+        static Random random = new Random();
+
         public Game(List<Client> clients)
         {
             this.clients = clients;
+            state = TurnState.rotatePresident;
         }
 
         public void start()
         {
-            Console.WriteLine("Game has started with the following players:");
-            foreach (var client in clients)
+            Client.SendMessageToAllClients(Message.gameStart());
+            currentPresident = pickRandomPresident();
+            
+        }
+
+        // first president is randomly selected
+        public int pickRandomPresident()
+        {
+            int index = random.Next(clients.Count);
+            Message.becomePresident(clients[index].player);
+            return index;
+        }
+
+        //the presidency moves to the next player in line
+        public Client rotatePresident(int number)
+        {
+            if (number != clients.Count - 1)
             {
-                Console.WriteLine(client.player.name);
+                number = number + 1;
             }
+            else
+            {
+                number = 0;
+            }
+
+            Message.becomePresident(clients[number].player);
+            return clients[number];
         }
     }
 }
